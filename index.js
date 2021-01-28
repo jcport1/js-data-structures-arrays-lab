ff48bf1b6c01b0a9caa9d84880ae21d48ac2ddfb
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-28 15:42:16 -0500

    Testing

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+//push is destructive *adds an element to end of arrray*
+
+function destructivelyAppendDriver (name) { 
+    drivers.push(name); 
+}
+
+//unshift is destructive *adds element to beginning of array*
+
+function destructivelyPrependDriver (name) {
+     drivers.unshift(name);
+}
+
+// pop destructively removes last element
+
+function destructivelyRemoveLastDriver () {
+    drivers.pop();
+}
+
+//shift destructively removes first element
+
+function destructivelyRemoveFirstDriver () {
+    drivers.shift();
+}
+
+//Non-destructive add & remove
+
+//... adds nondestructively 
+
+function appendDriver (name) {
+    return [...drivers, name];
+}
+
+function prependDriver(name) {
+    return [name, ...drivers];
+}
+
+//slice removes non-destructively 
+function removeFirstDriver () {
+
+    return drivers.slice(1);
+
+}
+
+function removeLastDriver () {
+    
+    return drivers.slice(0, drivers.length - 1);
+
+}
